bbcd8997aaf785517d7a86fcdc3dfd6d9c47dbd3
Andrea Fortuna <andrea.fortuna@gmail.com>
2013-11-07 22:35:49 +0100

    some fixes

---
+++ dp-config.php
@@ -0,0 +1,27 @@
+<?php
+/*******************************
+*	DumbPress Configuration file
+*******************************/
+
+/*****************
+* Database access
+******************/
+
+$dbhost = "localhost";
+$dbname = "dplit";
+$dbusername = "root";
+$dbpassword = "";
+
+/*****************
+* Admin Account
+******************/
+
+$adminusername = "admin";
+$adminpassword = "password";
+
+/*******************
+* ADVANCED SETTINGS 
+*******************/
+$enableDebug = "1";
+
+?>
